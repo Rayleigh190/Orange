ed7f194b66ae84edfbeb2d7de25d7b9293b660f0
rayleigh <rayleigh190@gmail.com>
2022-08-01 00:15:48 +0900

    feat: 로그인 api 구현 완료

--- common/serializers.py
+++ common/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
-# from django.contrib.auth import authenticate 
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from rest_framework.authtoken.models import Token
@@ -42,17 +42,17 @@ class RegisterSerializer(serializers.ModelSerializer): # 회원가입 시리얼�
         return user
 
 
-# class LoginSerializer(serializers.Serializer):
-#     username = serializers.CharField(required=True)
-#     password = serializers.CharField(required=True, write_only=True)
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
 
-#     def validate(self, data):
-#         user = authenticate(**data)
-#         if user:
-#             token = Token.objects.get(user=user)
-#             return token
-#         raise serializers.ValidationError(
-#             {"error": "Unable to log in with provided credentials."})
+    def validate(self, data):
+        user = authenticate(**data)
+        if user:
+            token = Token.objects.get(user=user)
+            return token
+        raise serializers.ValidationError(
+            {"error": "Unable to log in with provided credentials."})
 
 
 # class ProfileSerializer(serializers.ModelSerializer):
