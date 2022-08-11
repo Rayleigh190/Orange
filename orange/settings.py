a4136ad7fceea3319f8dbbc3aa36004963097f7a
rayleigh <rayleigh190@gmail.com>
2022-08-11 11:26:18 +0900

    fix: delete tkinter & set STATICFILES DIRS

--- orange/settings.py
+++ orange/settings.py
@@ -15,8 +15,6 @@ import os, json
 from tkinter.tix import Tree
 from django.core.exceptions import ImproperlyConfigured
 
-from tkinter import TRUE
-
 # mysql 호환용
 import pymysql
 pymysql.install_as_MySQLdb()
@@ -161,10 +159,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-
-cleanup_data = TRUE
