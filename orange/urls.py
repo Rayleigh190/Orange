387da88d07d7734966b5d9091785b8b657de17ec
rayleigh <rayleigh190@gmail.com>
2022-08-11 16:03:26 +0900

    fix: change media urlpatterns

--- orange/urls.py
+++ orange/urls.py
@@ -18,12 +18,19 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
-urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('common.urls')),
     path('inner/', include('persona.urls')),
     path('outer/', include('persona.urls')),
     path('prism/', include('persona.urls')),
     path('recommendation/', include('persona.urls')),
-]
+] 
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns+=url(r'^media/(?P<path>.\*)$', serve, {
+    'document_root': settings.MEDIA_ROOT,
+})
